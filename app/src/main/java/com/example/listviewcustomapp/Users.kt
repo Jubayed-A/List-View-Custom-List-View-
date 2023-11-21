7c5b663f76d2e7414faf76ba89d0dc8325922cac
Jubayed-A <learnigprograming@gmail.com>
2023-11-22 01:09:43 +0600

    User data class create and data assign

--- app/src/main/java/com/example/listviewcustomapp/Users.kt
+++ app/src/main/java/com/example/listviewcustomapp/Users.kt
@@ -1,3 +1,5 @@
 package com.example.listviewcustomapp
 
-data class Users()
+data class Users(var name : String, var lastMsg : String, var lastMsgTime : String, var phoneNum : String, var imgId : Int){
+
+}
